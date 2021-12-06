7e82ebb26a7d895b35f0d9e325254e5cecb793fa
DeepakPradhan96 <92121710+DeepakPradhan96@users.noreply.github.com>
2021-12-06 23:04:27 +0530

    Update StepDefination.py

--- Feature/Steps/StepDefination.py
+++ Feature/Steps/StepDefination.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-# from selenium.webcontext.driver.common.keys import keys, Keys
+
 import pyperclip
 
 
