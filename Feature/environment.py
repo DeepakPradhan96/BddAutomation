2d4459988f5a8ec199b9e9d565d16b3a36b1fbfb
DeepakPradhan96 <deeep040@gmail.com>
2021-12-06 19:49:59 +0530

    new add 6.12.21

--- Feature/environment.py
+++ Feature/environment.py
@@ -1,9 +1,13 @@
 from selenium.webdriver import Chrome
+from selenium.webdriver import  Firefox
 #context= global/use entair project
-
+from selenium import webdriver
 def before_all(context):
     path='C:\\Driver\\chromedriver_win32\\chromedriver.exe'
     context.driver =Chrome(executable_path=path)
+
+
+    context.driver2 = webdriver.Firefox()
 
 
 def after_all(context):
@@ -11,3 +15,4 @@ def after_all(context):
 #def after_scenario(context,scenario):
 #def after_tag,step(context,tag):
     context.driver.close()
+
